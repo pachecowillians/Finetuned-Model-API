0292eddbd2a6a0c5a94cc181cf89244c49ed1132
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-01-27 00:42:24 -0300

    refactor: changing tokenizer default model name

--- app/utils/tokenizer.py
+++ app/utils/tokenizer.py
@@ -1,7 +1,7 @@
 from transformers import DistilBertTokenizer
 
 class Tokenizer:
-    def __init__(self, model_name="distilbert-base-uncased"):
+    def __init__(self, model_name="distilbert-base-uncased-finetuned-sst-2-english"):
         self.tokenizer = DistilBertTokenizer.from_pretrained(model_name)
     
     def tokenize(self, text):
