78de89a0082fa726e46b2532e843595e3059fff8
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-01-27 07:39:36 -0300

    feat: setting host

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.register_blueprint(classifier_api, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True, host="0.0.0.0")
